8e88eec20b7bbba8819865737c5247fbacc7d28d
BHUVANESH G <140417490+BHUVANESH-06@users.noreply.github.com>
2024-09-22 11:18:18 +0530

    Details_Committed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Recommendation from './Recommendation';
-import './App.css';
 import Top from './Top';
-import Image from './Assets/logo.png'
+import Related from './Related';
+import './App.css';
+import Image from './Assets/logo.png';
 
 function App() {
     return (
-        <>
+        <Router>
             <div className='nav'>
-                <img src={Image} alt="logo" className="logo"/>
+                <img src={Image} alt="logo" className="logo" />
                 <div className='menu'>
                     <h3>UNLIMITED TV SHOWS & MOVIES</h3>
                     <button className='join'>Join now</button>
@@ -19,21 +21,26 @@ function App() {
                 <h1>Only on Netflix</h1>
                 <h3>Netflix is the home of amazing original programming that you can’t find anywhere else. Movies, TV shows, specials and more, all tailored specifically to you.</h3>
             </div>
-            <div className="rows">
-                <h5>Movies based on your interest</h5>
-                <Recommendation />
-                <h5>Science Fiction</h5>
-                <Top genre="sciencefiction"/>
-                <h5>Animation</h5>
-                <Top genre="animation"/>
-                <h5>Fantasy</h5>
-                <Top genre="fantasy"/>
-                <h5>Romantic</h5>
-                <Top genre="romance"/>
-                <h5>Action</h5>
-                <Top genre="action"/>
-            </div>
-        </>
+                <Routes>
+                    <Route path='/' element={
+                        <div className="rows">
+                            <h5>Movies based on your interest</h5>
+                            <Recommendation />
+                            <h5>Science Fiction</h5>
+                            <Top genre="sciencefiction" />
+                            <h5>Animation</h5>
+                            <Top genre="animation" />
+                            <h5>Fantasy</h5>
+                            <Top genre="fantasy" />
+                            <h5>Romantic</h5>
+                            <Top genre="romance" />
+                            <h5>Action</h5>
+                            <Top genre="action" />
+                        </div>
+                    }/>
+                    <Route path="/related/:movieTitle" element={<Related />} />
+                </Routes>
+        </Router>
     );
 }
 
